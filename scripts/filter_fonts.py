068959f156c85d01ea8193bf37a9db7be18925c1
ad220 <dev@ad220.fr>
2025-10-23 21:18:14 +0200

    add fonts to match the scale of any device screen

---
+++ scripts/filter_fonts.py
@@ -0,0 +1,62 @@
+import sys
+import glob
+
+
+def sort_filter(filter):
+    return "".join(sorted(set("".join(filter))))
+
+
+def extract_word(line:str): 
+    label = line.split('<')[1].split('>')[1]
+    return "".join(label.split("\\n"))
+
+
+def filter(filename:str, translatable:bool):
+    with open(filename, 'r', encoding="utf-8") as f:
+        lines = f.readlines()
+        small_filter = [" 0123456789-:"]
+        medium_filter = [" .0123456789-:'"]
+        large_filter = [" .0123456789-"]
+        current_filters = []
+        for line in lines:
+            if '$$$' in line:
+                current_filters = []
+                if 'Small' in line:
+                    current_filters.append(small_filter)
+                if 'Medium' in line:
+                    current_filters.append(medium_filter)
+                if 'Large' in line:
+                    current_filters.append(large_filter)
+
+            if 'string id' in line:
+                if translatable != ('translatable="false"' in line):
+                    word = extract_word(line)
+                    for filter in current_filters:
+                        filter += word
+
+    return sort_filter(small_filter), sort_filter(medium_filter), sort_filter(large_filter)
+
+
+def filter_all(rootpath):
+    files = glob.glob('**/strings.xml', root_dir=rootpath, recursive=True)
+
+    not_translatables = filter(files[0], False)
+    global_filter = [f for f in not_translatables]
+    for f in files:
+        filters = filter(f, True)
+        print(f, {
+            "small":    sort_filter(filters[0]+not_translatables[0]),
+            "medium":   sort_filter(filters[1]+not_translatables[1]),
+            "large":    sort_filter(filters[2]+not_translatables[2])
+        })
+        for i in range(3):
+            global_filter[i] = sort_filter(filters[i]+global_filter[i])
+    print("global", {
+        "small":    global_filter[0],
+        "medium":   global_filter[1],
+        "large":    global_filter[2]
+    })
+
+
+if __name__ == '__main__':
+    filter_all(sys.argv[1])
--- source/interface/InterfaceComponentsManager.mc
+++ source/interface/InterfaceComponentsManager.mc
@@ -8,10 +8,6 @@ module InterfaceComponentsManager {
     
     var screenH as Number?;
     var screenW as Number?;
-    var halfH as Number?;
-    var halfW as Number?;
-    var kMult as Float?; // compared to 240x240 screen
-    var imgOff as Float?;
 
     var fontTiny as FontResource?;
     var fontSmall as FontResource?;
@@ -26,11 +22,6 @@ module InterfaceComponentsManager {
         var deviceSettings = System.getDeviceSettings();
         screenH = deviceSettings.screenHeight;
         screenW = deviceSettings.screenWidth;
-        halfH = screenH / 2;
-        halfW = screenW / 2;
-        kMult = (screenH / 120)*0.5;
-        if (kMult < 1) {kMult = 1.0;}
-        imgOff = 0.05*screenH-12*kMult;
     }
 
     function scaleX(xPercent as Float) as Number {
@@ -42,7 +33,6 @@ module InterfaceComponentsManager {
     }
 
     function loadFonts() as Void{
-        fontTiny = WatchUi.loadResource(Rez.Fonts.Tiny);
         fontSmall = WatchUi.loadResource(Rez.Fonts.Small);
         fontMedium = WatchUi.loadResource(Rez.Fonts.Medium);
         fontLarge = WatchUi.loadResource(Rez.Fonts.Large);
@@ -53,14 +43,5 @@ module InterfaceComponentsManager {
         fontSmall = null;
         fontMedium = null;
         fontLarge = null;
-    }
-    
-    // TODO: replace with font resource override by screen size
-    function adaptFontSmall() as FontResource {
-        return kMult<=1 ? fontTiny : fontSmall;
-    }
-
-    public function adaptFontMid() as FontResource {
-        return kMult<=1 ? fontSmall : fontMedium;
     }
 }
